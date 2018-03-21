7bc197e72c763444bd93f43c76c8f1c49be5f5c4
titanium99 <planet.titan@gmail.com>
2018-03-21 02:22:34 +0000

    Add heroku setting

--- app.py
+++ app.py
@@ -75,4 +75,14 @@ def callback():
         abort(400)
     return 'OK'
 
-run(host='0.0.0.0', port=8081, debug=True)
+
+appenv = os.getenv('APP_ENV', None)
+
+if appenv != 'heroku':
+    host = "0.0.0.0"
+    port = 8081
+else:
+    host = os.getenv('HOST', None)
+    port = os.getenv('PORT', None)
+
+run(host=host, port=port, debug=True)
